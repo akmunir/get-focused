a90a92ecb56fc1c1e5c9a696986e30b2cc04da13
Asim <akm02145@gmail.com>
2025-04-08 22:42:00 -0400

    Working on inputting time and properly formatting it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles.css';
+
+const timerEl = document.querySelector("#timer");
+const currentTime = timerEl.textContent.toString();
+const timeInput = document.querySelector("#time");
+console.log(timeInput.value);
+
+addEventListener("focusout", formatInput);
+
+function formatInput(event) {
+    const totalTime = Number(event.target.value);
+    const minutes = Math.floor(totalTime / 60);
+    if (minutes > 60) {
+        const hours = Math.floor(minutes / 60);
+        minutes = minutes % 60;
+    } else {
+        hours = 0;
+    }
+    const seconds = totalTime % 60;
+    event.target.input
+}
